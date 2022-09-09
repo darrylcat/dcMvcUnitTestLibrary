7874d7061d2aa59d435cb1267eb334b734c88d9a
Darryl Catchpole <109890938+darrylcat@users.noreply.github.com>
2022-09-09 17:03:20 +0100

    Starting the unit test development
    Adding a unit test to Home Controller, so we can start building our helper kit.

---
+++ DemoBlogAppTests/Controllers/HomeControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using DemoBlogApp.Controllers;
+using DemoBlogAppTests.TestUtilities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DemoBlogAppTests.Controllers
+{
+    [TestClass()]
+    public class HomeControllerTest : UnitTestBase
+    {
+
+        private static HomeController testObj { get; set; }
+
+        [TestInitialize()]
+        public override void Setup()
+        {
+            base.Setup();
+            if(testObj == null)
+            {
+                testObj = new HomeController(HomeLogger, dbContextFactory);
+            }
+        }
+
+        [TestMethod()]
+        public void IsObject()
+        {
+            Assert.IsInstanceOfType(testObj, typeof(HomeController));
+        }
+
+        [TestMethod()]
+        public void ImplementsController()
+        {
+            Assert.IsTrue(testObj is Controller);
+        }
+
+
+    }
+}
--- DemoBlogAppTests/TestUtilities/UnitTestBase.cs
+++ DemoBlogAppTests/TestUtilities/UnitTestBase.cs
@@ -1,13 +1,36 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using DemoBlogApp.Controllers;
+using DemoBlogApp.Models.Database;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Moq;
 
 namespace DemoBlogAppTests.TestUtilities
 {
     public class UnitTestBase
     {
+        protected ILogger<HomeController> HomeLogger { get; set; }
+        protected IDbContextFactory<DemoBlogContext> dbContextFactory { get; set; }
+
         public virtual void Setup()
-        { }
+        {
+            InitaliseLoggers();
+            InitialseDatabaseFactory();
+        }
+
+        private void InitialseDatabaseFactory()
+        {
+            var mockDbContextFactory = new Mock<IDbContextFactory<DemoBlogContext>>();
+            dbContextFactory = mockDbContextFactory.Object;
+        }
+
+        private void InitaliseLoggers()
+        {
+            var mockLogger = new Mock<ILogger<HomeController>>();
+            HomeLogger = mockLogger.Object;
+        }
 
         public virtual void Cleanup()
         { }
