9d9482132e232dd17e257dfd0d3b41319c1ad9f7
Darryl Catchpole <109890938+darrylcat@users.noreply.github.com>
2022-09-10 14:31:51 +0100

    Core Service Functions
    Moved core service functions into test library.

--- DemoBlogAppTests/TestUtilities/UnitTestBase.cs
+++ DemoBlogAppTests/TestUtilities/UnitTestBase.cs
@@ -9,44 +9,29 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Moq;
+using UnitTestHelperLibrary;
 using UnitTestHelperLibrary.Services;
 
 namespace DemoBlogAppTests.TestUtilities
 {
-    public class UnitTestBase
+    public class UnitTestBase : UnitTestCore
     {
         protected string CONNECTION_STRING = "Data Source= :memory:;Version=3;New=True;";
 
         protected static ILogger<HomeController> HomeLogger { get; set; }
         protected static IDbContextFactory<DemoBlogContext> dbContextFactory { get; set; }
-        protected static IServiceCollection ServiceCollection { get; set; }
-        protected static IServiceProvider ServiceProvider { get; private set; }
 
-        public virtual void Setup()
+        public override void Setup()
         {
-            InitialiseServiceCollection();
+            base.Setup();
             InitaliseLoggers();
             InitialseDatabaseFactory();
         }
 
-        private void InitialiseServiceCollection()
+        protected override void AddServices()
         {
-            if (ServiceCollection == null)
-            {
-                ServiceCollection = ServiceCollectionFactory.Create();
-                AddServices();
-                InitialiseServiceProvider();
-            };
-        }
-
-        private void InitialiseServiceProvider()
-        {
-            ServiceProvider = ServiceCollection.BuildServiceProvider();
-        }
-
-        private void AddServices()
-        {
-            ServiceCollection.AddLogging();
+            base.AddServices();
+            // Add Application specific services
             ServiceCollection.AddDbContextFactory<DemoBlogContext>(opt =>
                     opt.UseInMemoryDatabase(CONNECTION_STRING)
                     .EnableSensitiveDataLogging()
@@ -61,22 +46,11 @@ namespace DemoBlogAppTests.TestUtilities
             mockDbContextFactory.Setup(f => f.CreateDbContext())
                 .Returns(new DemoBlogContext(options));
             dbContextFactory = mockDbContextFactory.Object; */
-            dbContextFactory = ServiceProvider.GetService<IDbContextFactory<DemoBlogContext>>();            
-        }
-
-        private DbContextOptions GetConnectionOptions(string connectionString)
-        {
-            var opt = new DbContextOptionsBuilder()
-                .UseInMemoryDatabase(connectionString)
-                .EnableSensitiveDataLogging()
-                .Options;
-            return opt;
+            dbContextFactory = GetDbContextFactory<DemoBlogContext>(); 
         }
 
         private void InitaliseLoggers()
         {
-            // var mockLogger = new Mock<ILogger<HomeController>>();
-            // HomeLogger = mockLogger.Object;
             var factory = ServiceProvider.GetService<ILoggerFactory>();
             HomeLogger = factory.CreateLogger<HomeController>();
         }
---
+++ UnitTestHelperLibrary/UnitTestCore.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using UnitTestHelperLibrary.Services;
+
+namespace UnitTestHelperLibrary
+{
+    public abstract class UnitTestCore
+    {
+        protected static IServiceCollection ServiceCollection { get; set; }
+        protected static IServiceProvider ServiceProvider { get; private set; }
+
+        public UnitTestCore()
+        {
+        }
+
+        public virtual void Setup()
+        {
+            InitialiseServiceCollection();
+        }
+
+        protected void InitialiseServiceProvider()
+        {
+            if(ServiceProvider == null)
+            {
+                ServiceProvider = ServiceCollection.BuildServiceProvider();
+            }
+        }
+
+        private void InitialiseServiceCollection()
+        {
+            if (ServiceCollection == null)
+            {
+                ServiceCollection = ServiceCollectionFactory.Create();
+                AddServices();
+                InitialiseServiceProvider();
+            };
+        }
+
+        /*
+         *  override AddServices to include any application specific services such as IDbContextFactories 
+         */
+        protected virtual void AddServices()
+        {
+            ServiceCollection.AddLogging();
+        }
+
+        protected IDbContextFactory<TContext> GetDbContextFactory<TContext>() where TContext : DbContext
+        {
+            var dbFactory = ServiceProvider.GetService<IDbContextFactory<TContext>>();
+            return dbFactory;
+        }
+
+
+    }
+}
