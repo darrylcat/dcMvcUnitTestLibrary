c28b15674a3409fde1b0694bb3a669ec47bde509
Darryl Catchpole <109890938+darrylcat@users.noreply.github.com>
2022-09-18 11:06:20 +0100

    Create MvcToolkit.cs
    Adding toolkit for MvcTesting

---
+++ UnitTestHelperLibrary/ViewTools/MvcToolkit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UnitTestHelperLibrary.ViewTools
+{
+    public class MvcToolkit
+    {
+        public static bool IsViewRecord(object actual)
+        {
+            return (actual is ViewResult);
+        }
+    }
+}
