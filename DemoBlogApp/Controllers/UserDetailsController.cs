3033c4127a1648d1a645e0ed376794c5c5fbced9
Darryl Catchpole <cjll1234@hotmail.com>
2022-09-18 09:42:17 +0100

    Added user detail controller

---
+++ DemoBlogApp/Controllers/UserDetailsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using DemoBlogApp.Models.Database;
+
+namespace DemoBlogApp.Controllers
+{
+    public class UserDetailsController : Controller
+    {
+        private readonly DemoBlogContext _context;
+
+        public UserDetailsController(IDbContextFactory<DemoBlogContext> dbContextFactory)
+        {
+            _context = dbContextFactory.CreateDbContext();
+        }
+
+        // GET: UserDetails
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.UserDetails.ToListAsync());
+        }
+
+        // GET: UserDetails/Details/5
+        public async Task<IActionResult> Details(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userDetail = await _context.UserDetails
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userDetail == null)
+            {
+                return NotFound();
+            }
+
+            return View(userDetail);
+        }
+
+        // GET: UserDetails/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: UserDetails/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Title,FirstName,LastName,Email,Password,Salt,Created,Active")] UserDetail userDetail)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(userDetail);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userDetail);
+        }
+
+        // GET: UserDetails/Edit/5
+        public async Task<IActionResult> Edit(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userDetail = await _context.UserDetails.FindAsync(id);
+            if (userDetail == null)
+            {
+                return NotFound();
+            }
+            return View(userDetail);
+        }
+
+        // POST: UserDetails/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(long id, [Bind("Id,Title,FirstName,LastName,Email,Password,Salt,Created,Active")] UserDetail userDetail)
+        {
+            if (id != userDetail.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userDetail);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserDetailExists(userDetail.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userDetail);
+        }
+
+        // GET: UserDetails/Delete/5
+        public async Task<IActionResult> Delete(long? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var userDetail = await _context.UserDetails
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (userDetail == null)
+            {
+                return NotFound();
+            }
+
+            return View(userDetail);
+        }
+
+        // POST: UserDetails/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(long id)
+        {
+            var userDetail = await _context.UserDetails.FindAsync(id);
+            _context.UserDetails.Remove(userDetail);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserDetailExists(long id)
+        {
+            return _context.UserDetails.Any(e => e.Id == id);
+        }
+    }
+}
