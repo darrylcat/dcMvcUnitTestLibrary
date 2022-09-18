8d3d38160457fc42d746b0a23203d4cfadcd3639
Darryl Catchpole <cjll1234@hotmail.com>
2022-09-18 10:15:56 +0100

    Added unit tests for UserDetails controller

---
+++ DemoBlogAppTests/Controllers/UserDetailsControllerTests.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DemoBlogApp.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoBlogApp.Controllers.Tests
+{
+    [TestClass()]
+    public class UserDetailsControllerTests
+    {
+        [TestMethod()]
+        public void UserDetailsControllerTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void IndexTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DetailsTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void EditTest1()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteConfirmedTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
